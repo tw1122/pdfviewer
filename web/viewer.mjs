a0c84fafe1e60452250015d1c119d6e1f4d1c446
tw1122 <taiki_watanabe@yamate-gakuin.ed.jp>
2025-03-27 23:58:37 +0900

    Create viewer.mjs

---
+++ web/viewer.mjs
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PDF.js viewer.mjs モジュール版</title>
+  <link rel="stylesheet" href="viewer.css" />
+</head>
+<body>
+
+<div id="viewerContainer" class="pdfViewer"></div>
+
+<script type="module" src="viewer.mjs"></script>
+
+</body>
+</html>
